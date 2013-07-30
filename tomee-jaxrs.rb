9d8c2b6b27d43ec58295e3afb93ba0dcf0b287cd
Michael Woods <mwoods@grassroots-tech.com>
2013-07-30 15:59:23 -0400

    Initial jaxrs profile of tomee, just a copy of webprofile with URLs and sha changed. Plus a catalina symlink.

---
+++ tomee-jaxrs.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class TomeeJaxrsprofile < Formula
+  homepage 'http://tomee.apache.org/'
+  url 'http://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.5.2/apache-tomee-1.5.2-jaxrs.tar.gz'
+  version '1.5.2'
+  sha1 'fae137442779365f80e54d2df6beab9c024326e3'
+
+  # Keep log folders
+  skip_clean 'libexec'
+
+  def install
+    # Remove Windows scripts
+    rm_rf Dir['bin/*.bat']
+
+    # Install files
+    prefix.install %w{ NOTICE LICENSE RELEASE-NOTES RUNNING.txt }
+    libexec.install Dir['*']
+    bin.install_symlink "#{libexec}/bin/tomee.sh" => "tomee-jaxrsprofile"
+    bin.install_symlink "#{libexec}/bin/catalina.sh" => "catalina"
+  end
+end
